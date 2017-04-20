64731a438eae8ce21f30e39178a423edfbcdd3e6
Jacob McIntosh <nacitar@ubercpp.com>
2017-04-20 15:56:28 -0500

    sorting ram fields

--- ram.lua
+++ ram.lua
@@ -78,26 +78,28 @@ return {
 
   rng = Field(0x7E0FA0, 2, 'u'),
 
-  player_x = Field(0x7E0022, 2, 'u'),
-  player_x_cycle = Field(0x7E0031, 1, 's'),
-  player_x_cycle_index = Field(0x7E002B, 1, 'u'),
-
+  player_not_overworld = Field(0x7E001B, 1, 'u'),
+      -- 0x0 = overworld, 0x1 = house or dungeon
   player_y = Field(0x7E0020, 2, 'u'),
-  player_y_cycle = Field(0x7E0030, 1, 's'),
+  player_x = Field(0x7E0022, 2, 'u'),
+  input_push_state = Field(0x7E0026, 1, 'u'),
+      -- TODO: useful? looks like input_buffer_main except only udlr
   player_y_cycle_index = Field(0x7E002A, 1, 'u'),
-
+  player_x_cycle_index = Field(0x7E002B, 1, 'u'),
+  animation_step_counter = Field(0x7E002E, 1, 'u'),
   player_facing = Field(0x7E002F, 1, 'u'),
-  player_on_lower_level = Field(0x7E00EE, 1, 'u'),
-      -- 0 upper, 1 lower
+  player_y_cycle = Field(0x7E0030, 1, 's'),
+  player_x_cycle = Field(0x7E0031, 1, 's'),
+
+
   player_movement_type = Field(0x7E005E, 1, 'u'),
       -- 0x0C == sword/item held
       -- 0x00 == normal
       -- 0x10 == dashing (not on stairs)
       -- 0x02 == stairs (when walking/dashing vertically)
       -- 0x06 == entering cave from overworld
-  player_not_overworld = Field(0x7E001B, 1, 'u'),
-      -- 0x0 = overworld, 0x1 = house or dungeon
-  animation_step_counter = Field(0x7E002E, 1, 'u'),
+  player_on_lower_level = Field(0x7E00EE, 1, 'u'),
+      -- 0 upper, 1 lower
 
   input_buffer_main = Field(0x7E00F0, 1, 'u'),  -- BYST|udlr
   input_buffer_main_flags = {
@@ -117,6 +119,4 @@ return {
     X = 64,
     A = 128,
   },
-  input_push_state = Field(0x7E0026, 1, 'u'),
-      -- TODO: useful? looks like JP1_PRESSED_01 except only udlr
 }
