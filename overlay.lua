73c2f9d7d2b46dc474e999e783cb88aeceb76656
Jacob McIntosh <nacitar@ubercpp.com>
2017-08-10 21:42:59 -0500

    EG detection is better now

--- overlay.lua
+++ overlay.lua
@@ -1,63 +1,66 @@
 local ram = require 'ram'
 local util = require 'util'
 
-
 local var = {
   -- can be > 1 if it's queued and then you jump... code just adds 1 to it for
   -- some reason, rather than setting to 1 like a bool
   queued_layer_change = ram.Unsigned(0x7E047A, 2),
   room_upper_layer = ram.Unsigned(0x7E044A, 2),
-  falling_state = ram.Unsigned(0x7E005B, 1),
+  pseudo_bg_level = ram.Unsigned(0x7E0476, 2),
   player_state = ram.Unsigned(0x7E005D, 1),
 }
-local FallingStateFlags = {
-  NORMAL = 0,
-  NEAR_PIT = 1,
-  FALLING_IN = 2,
-  FALLING_OUT = 3,
-}
+
 local PlayerStateFlags = {
-  FALLING_OR_NEAR_PIT = 1,
   JUMPING = 6,
 }
 
+local StoredEG = {
+  DISARMED = 0,
+  STRONG = 1,
+  WEAK = 2,
+}
 
-local eg_status = 0
-
-local last_state = nil
-
-function main()
-  queued_layer_change = var.queued_layer_change:read()
+local stored_eg = nil
+local last_player_state = nil
+function update_stored_eg()
   player_state = var.player_state:read()
-  room_upper_layer = var.room_upper_layer:read()
-  if queued_layer_change == 0 then
-    eg_status = nil
-  else
-    falling_state = var.falling_state:read()
-    -- the last frame of a jump swaps the state before clearing the queued
-    -- layer change value... so we have to ignore the first call for accuracy
-    -- sake, as we must check the previous player state.
-    -- also, if we're currently jumping or currently falling, don't bother
-    -- because we can't be sure of the state
-    if last_player_state ~= nil and
+  if last_player_state ~= nil then
+    queued_layer_change = var.queued_layer_change:read()
+    -- if we jump when we already have stored EG, the count will be > 1, keep
+    -- the indicator in that case... but if it is only 1, we need to make sure
+    -- it's not just a normal jump.  Also, the last frame of a jump to the
+    -- ground swaps the state one frame prior to clearing the
+    -- queued_layer_change, so we have to backlog one frame. =/
+    if queued_layer_change > 1 or (queued_layer_change == 1 and
+        last_player_state ~= nil and
         last_player_state ~= PlayerStateFlags.JUMPING and
-        player_state ~= PlayerStateFlags.JUMPING and (
-        player_state ~= PlayerStateFlags.FALLING_OR_NEAR_PIT or (
-          falling_state == FallingStateFlags.NORMAL or
-          falling_state == FallingStateFlags.NEAR_PIT)) then
-      eg_status = room_upper_layer
+        player_state ~= PlayerStateFlags.JUMPING) then
+      room_upper_layer = var.room_upper_layer:read()
+      if room_upper_layer == 1 then
+        stored_eg = StoredEG.STRONG
+      else
+        stored_eg = StoredEG.WEAK
+      end
+    else
+      stored_eg = StoredEG.DISARMED
     end
   end
   last_player_state = player_state
 end
 
-function eg_status_string()
-  if eg_status == 1 then
+function main()
+  update_stored_eg()
+end
+
+function stored_eg_string()
+  if stored_eg == StoredEG.STRONG then
     return 'strong'
-  elseif eg_status ~= nil then
+  elseif stored_eg == StoredEG.WEAK then
     return 'weak'
+  elseif stored_eg == StoredEG.DISARMED then
+    return 'disarmed'
   end
-  return 'disarmed'
+  return ''
 end
 
 local function draw_text(base_x, base_y, lines)
@@ -69,7 +72,7 @@ end
 
 while true do
   main()
-  status = eg_status_string()
+  status = stored_eg_string()
   draw_text(2, 120, {'Stored EG: ' .. status})
   snes9x.frameadvance()
 end
