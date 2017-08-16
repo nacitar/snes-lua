bf342dd12ea295c689a5d21d35618d5893abbf84
Jacob McIntosh <nacitar@ubercpp.com>
2017-08-16 14:41:37 -0500

    Separated out class.

---
+++ class.lua
@@ -0,0 +1,89 @@
+#!/usr/bin/env lua
+
+-- class.lua
+-- Compatible with Lua 5.1 (not 5.0).
+--
+-- class() uses two tricks. It allows you to construct a class using the call
+-- notation (like Dog('fido')) by giving the class itself a metatable
+-- which defines __call. It handles inheritance by copying the fields of the
+-- base class into the derived class. This isn't the only way of doing
+-- inheritance; we could make __index a function which explicitly tries to
+-- look a function up in the base class(es). But this method will give better
+-- performance, at a cost of making the class objects somewhat fatter. Each
+-- derived class does keep a field _base that contains the base class, but
+-- this is to implement is_a.
+--
+-- Note that modification of a base class at runtime will not affect its
+-- subclasses.
+--
+--
+-- -- EXAMPLE
+--
+--  local util = require 'util'
+--
+--  A = util.class()
+--  function A:__init(x)
+--    self.x = x
+--  end
+--  function A:test()
+--    print(self.x)
+--  end
+-- 
+--  function A:__add(b)
+--    return A(self.x + b.x)
+--  end
+-- 
+--  B = class(A)
+--  function B:__init(x,y)
+--    A.__init(self,x)
+--    self.y = y
+--  end
+-- 
+--  x=A(5)
+--  y=B(6)
+--  z=x + y
+--  z:test()
+--
+
+function class(base, __init)
+  local c = {}    -- a new class instance
+  if not __init and type(base) == 'function' then
+    __init = base
+    base = nil
+  elseif type(base) == 'table' then
+    -- our new class is a shallow copy of the base class!
+    for i,v in pairs(base) do
+      c[i] = v
+    end
+    c._base = base
+  end
+  -- the class will be the metatable for all its objects,
+  -- and they will look up their methods in it.
+  c.__index = c
+  -- expose a constructor which can be called by <classname>(<args>)
+  local mt = {}
+  mt.__call = function(class_tbl, ...)
+    local obj = {}
+    setmetatable(obj,c)
+    if class_tbl.__init then
+      class_tbl.__init(obj,...)
+    else
+      -- make sure that any stuff from the base class is initialized!
+      if base and base.__init then
+        base.__init(obj, ...)
+      end
+    end
+    return obj
+  end
+  c.__init = __init
+  c.is_a = function(self, klass)
+    local m = getmetatable(self)
+    while m do
+      if m == klass then return true end
+      m = m._base
+    end
+    return false
+  end
+  setmetatable(c, mt)
+  return c
+end
--- util.lua
+++ util.lua
@@ -32,94 +32,6 @@ function Set (list)
   return set
 end
 
--- class() uses two tricks. It allows you to construct a class using the call
--- notation (like Dog('fido')) by giving the class itself a metatable
--- which defines __call. It handles inheritance by copying the fields of the
--- base class into the derived class. This isn't the only way of doing
--- inheritance; we could make __index a function which explicitly tries to
--- look a function up in the base class(es). But this method will give better
--- performance, at a cost of making the class objects somewhat fatter. Each
--- derived class does keep a field _base that contains the base class, but
--- this is to implement is_a.
---
--- Note that modification of a base class at runtime will not affect its
--- subclasses.
-
--- class.lua
--- Compatible with Lua 5.1 (not 5.0).
-function class(base, __init)
-  local c = {}    -- a new class instance
-  if not __init and type(base) == 'function' then
-    __init = base
-    base = nil
-  elseif type(base) == 'table' then
-    -- our new class is a shallow copy of the base class!
-    for i,v in pairs(base) do
-      c[i] = v
-    end
-    c._base = base
-  end
-  -- the class will be the metatable for all its objects,
-  -- and they will look up their methods in it.
-  c.__index = c
-
-  -- expose a constructor which can be called by <classname>(<args>)
-  local mt = {}
-  mt.__call = function(class_tbl, ...)
-    local obj = {}
-    setmetatable(obj,c)
-    if class_tbl.__init then
-      class_tbl.__init(obj,...)
-    else
-      -- make sure that any stuff from the base class is initialized!
-      if base and base.__init then
-        base.__init(obj, ...)
-      end
-    end
-    return obj
-  end
-  c.__init = __init
-  c.is_a = function(self, klass)
-    local m = getmetatable(self)
-    while m do
-      if m == klass then return true end
-      m = m._base
-    end
-    return false
-  end
-  setmetatable(c, mt)
-  return c
-end
-
---  -- EXAMPLE
---
---  local util = require 'util'
---
---  A = util.class()
---  function A:__init(x)
---    self.x = x
---  end
---  function A:test()
---    print(self.x)
---  end
---
---  function A:__add(b)
---    return A(self.x + b.x)
---  end
---
---  B = class(A)
---  function B:__init(x,y)
---    A.__init(self,x)
---    self.y = y
---  end
---
---  x=A(5)
---  y=B(6)
---  z=x + y
---  z:test()
---
-
-
 function draw_text(base_x, base_y, lines)
   for i, line in pairs(lines) do
     local y = base_y + (8 * (i - 1))
@@ -129,7 +41,6 @@ end
 
 
 return {
-  class = class,
   pretty_string = pretty_string,
   Set = Set,
   draw_text = draw_text,
