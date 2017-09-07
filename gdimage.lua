6b11830bb55888bfa22ee9489db24c199c45bf4c
Jacob McIntosh <nacitar@ubercpp.com>
2017-09-06 21:14:19 -0500

    Now the palette is padded to 256 entries, because even though the documentation says otherwise, that's what's right...

--- gdimage.lua
+++ gdimage.lua
@@ -16,6 +16,8 @@ end
 
 GDImage = class()
 
+local NOT_TRANSPARENT = 0xFFFFFFFF
+
 function GDImage:__init()
   self:clear()
 end
@@ -26,8 +28,10 @@ function GDImage:clear()
   self.width = 0
   self.height = 0
   self.is_palette = false
+  self.transparent = NOT_TRANSPARENT
 end
 
+-- GD 1.0 is untested
 function GDImage:load_file(filename)
   local file = assert(io.open(filename, 'r'),
       'Error loading file: ' .. filename)
@@ -49,14 +53,17 @@ function GDImage:load_file(filename)
       local is_truecolor = read_BE(data:sub(offset + 1, offset + 1))
       assert(is_truecolor == 0,
           'Truecolor flag should not be set for palette image, but it is...')
-      self.count = read_BE(data:sub(offset + 2, offset + 3))
+      used_colors = read_BE(data:sub(offset + 2, offset + 3))
       -- what is this?!?! says it's a palette index, but it's huge!
       self.transparent = read_BE(data:sub(offset + 4, offset + 7))
       offset = offset + 7
-      for i = 1, self.count do
+      for i = 1, used_colors do
         table.insert(self.palette, read_BE(data:sub(offset + 1, offset + 4)))
         offset = offset + 4
       end
+      -- the palette is ALWAYS 256 colors in size, even if not all of those
+      -- are used.  Skip the rest, though.
+      offset = offset + (256 - used_colors) * 4
     else
       -- redundant flag; should always be 1 for truecolor
       local is_truecolor = read_BE(data:sub(offset + 1, offset + 1))
@@ -72,15 +79,20 @@ function GDImage:load_file(filename)
     self.is_palette = true
     self.width = signature  -- not really a signature in 1.0
     self.height = read_BE(data:sub(3, 4))
-    self.count = read_BE(data:sub(5, 5))
-    -- what is this?  257 signals no transparency
+    used_colors = read_BE(data:sub(5, 5))
     self.transparent = read_BE(data:sub(6, 7))
+    if self.transparent == 257 then
+      self.transparent = NOT_TRANSPARENT
+    end
     offset = 7
-    for i = 1, self.count do
+    for i = 1, used_colors do
       -- only RGB
       table.insert(self.palette, read_BE(data:sub(offset + 1, offset + 3)))
       offset = offset + 3
     end
+    -- UNTESTED: i'm _assuming_ 1.0 palettes work the same as 2.0 ones, so
+    -- padding to 256 in size.
+    offset = offset + (256 - used_colors) * 3
   end
 
   -- pixel data
@@ -97,7 +109,44 @@ function GDImage:load_file(filename)
   end
 end
 
-function GDImage:save(filename)
+function GDImage:convert_to_truecolor()
+  if self.is_palette then
+    print('MOOO', #self.pixel_data)
+    self.is_palette = false
+    for i = 1, #self.pixel_data do
+      self.pixel_data[i] = self.palette[self.pixel_data[i] + 1]
+    end
+    if self.transparent ~= NOT_TRANSPARENT then
+      self.transparent = self.palette[self.transparent + 1]
+    end
+    self.palette = {}
+  end
+end
+
+function GDImage:convert_to_palette()
+  if not self.is_palette then
+    self.is_palette = true
+    self.palette = {}  -- unnecessary, probably
+    local color_map = {}
+    for i = 1, #self.pixel_data do
+      local color = self.pixel_data[i]
+      local index = color_map[color]
+      if index == nil then
+        index = #self.palette  -- lua index - 1
+        color_map[color] = index
+        table.insert(self.palette, color)
+      end
+      self.pixel_data[i] = index
+    end
+    if self.transparent ~= NOT_TRANSPARENT then
+      self.transparent = color_map[color]
+    end
+  end
+end
+
+
+
+function GDImage:save_file(filename)
   local file = assert(io.open(filename, 'wb'), 'Error opening: ' .. filename)
   file:write(write_BE(self.is_palette and 0xFFFF or 0xFFFE, 2))
   file:write(write_BE(self.width, 2))
@@ -106,8 +155,8 @@ function GDImage:save(filename)
     file:write(write_BE(0, 1))  -- palette
     file:write(write_BE(#self.palette, 2))
     file:write(write_BE(self.transparent, 4))
-    for i = 1, #self.palette do
-      file:write(write_BE(self.palette[i], 4))
+    for i = 1, 256 do  -- palette is always 256 in size, just not all used
+      file:write(write_BE(self.palette[i] or 0, 4))
     end
     for i = 1, #self.pixel_data do
       file:write(write_BE(self.pixel_data[i], 1))
@@ -122,9 +171,11 @@ function GDImage:save(filename)
   file:close()
 end
 
--- image = GDImage()
--- image:load_file(THIS_DIR .. '/foo.gd')
--- image:save(THIS_DIR .. '/test.gd')
+--image = GDImage()
+--image:load_file(THIS_DIR .. '/GD_example.gd')
+--image:convert_to_truecolor()
+--image:convert_to_palette()
+--image:save_file(THIS_DIR .. '/out.gd')
 
 return {
   GDImage = GDImage,
